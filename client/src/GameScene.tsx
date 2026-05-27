52fccdf1992ab2f106a571d56751047f98995418
MariuszADS <informatyka402@gmail.com>
2026-05-27 23:05:18 +0200

    fixed: GameConfig | filled_up: preload,create | added: gravity

--- client/src/GameScene.tsx
+++ client/src/GameScene.tsx
@@ -1,27 +1,80 @@
 import Phaser from 'phaser'
 
-const config = {
-  type: Phaser.AUTO,
-  width: 1000,
-  height: 600,
-  // scene: {
-  //   preload: preload,
-  //   create: create,
-  //   update: update
-  // }
-}
+let bird: Phaser.Physics.Arcade.Sprite
+let poles: Phaser.Physics.Arcade.Group
 
 
-const game = new Phaser.Game(config)
+const config: Phaser.Types.Core.GameConfig = {
+  physics: {
+    default: "arcade",
+    arcade: {
+      x: 1000,
+      y: 600,
+      gravity: {
+        x: 0,
+        y: 500
+      },
+      debug: true,
+      customUpdate: true,
+      //    checkCollision: {
+      //        up: true,
+      //        down: true,
+      //        left: true,
+      //        right: true
+      //    },
+      //    fixedStep: true,
+      //    fps: 60,
+      //    timeScale: 1,     // 2.0 = half speed, 0.5 = double speed
+      //    customUpdate: false,
+      //    overlapBias: 4,
+      //    tileBias: 16,
+      //    forceX: false,
+      //    isPaused: false,
+      //    debugShowBody: true,
+      //    debugShowStaticBody: true,
+      //    debugShowVelocity: true,
+      //    debugBodyColor: 0xff00ff,
+      //    debugStaticBodyColor: 0x0000ff,
+      //    debugVelocityColor: 0x00ff00,
+      //    maxEntries: 16,
+      //    useTree: true   // set false if amount of dynamic bodies > 5000
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+new Phaser.Game(config)
 
 function preload(this: Phaser.Scene) {
   this.load.image("background", "choose from assets")
+  this.load.image("bird", "choose from assets")
   this.load.image("pole", "choose from assets")
-  // this.load.spritesheet("dude","choose from assets") //is this need?
 }
 
-function create(this: Phaser.Scene, poles: Phaser.Physics.Arcade.StaticGroup) {
+/*
+gravity
+jump
+moving poles
+collision
+score
+ */
+function create(this: Phaser.Scene) {
   this.add.image(500, 500, "background")
-  poles = this.physics.add.staticGroup()
+  bird = this.physics.add.sprite(500, 500, "bird")
+  poles = this.physics.add.group()
+
+  //GRAVITY
+  this.physics.world.gravity.x;
+  this.physics.world.gravity.y;
+
+
+
 }
 
+function update(this: Phaser.Scene) {
+
+}
