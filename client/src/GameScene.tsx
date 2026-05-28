4255c84ee77d38116439bbefad287597f844a144
MariuszADS <informatyka402@gmail.com>
2026-05-28 23:30:16 +0200

    added: tsx syntax | first lunch

--- client/src/GameScene.tsx
+++ client/src/GameScene.tsx
@@ -1,80 +1,63 @@
-import Phaser from 'phaser'
+import{ useEffect, useRef } from 'react';
+import Phaser from 'phaser';
 
-let bird: Phaser.Physics.Arcade.Sprite
-let poles: Phaser.Physics.Arcade.Group
+export function GameScene() {
 
+  // game_div
+  const gameContainer = useRef<HTMLDivElement>(null);
 
-const config: Phaser.Types.Core.GameConfig = {
-  physics: {
-    default: "arcade",
-    arcade: {
-      x: 1000,
-      y: 600,
-      gravity: {
-        x: 0,
-        y: 500
+  // game_instance
+  const gameRef = useRef<Phaser.Game | null>(null);
+
+  // if game_div exist retrun current game_instance
+  useEffect(() => {
+    if (!gameContainer.current) return;
+
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: 1000,
+      height: 600,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 100, x: -10 },
+          debug: true,
+        },
       },
-      debug: true,
-      customUpdate: true,
-      //    checkCollision: {
-      //        up: true,
-      //        down: true,
-      //        left: true,
-      //        right: true
-      //    },
-      //    fixedStep: true,
-      //    fps: 60,
-      //    timeScale: 1,     // 2.0 = half speed, 0.5 = double speed
-      //    customUpdate: false,
-      //    overlapBias: 4,
-      //    tileBias: 16,
-      //    forceX: false,
-      //    isPaused: false,
-      //    debugShowBody: true,
-      //    debugShowStaticBody: true,
-      //    debugShowVelocity: true,
-      //    debugBodyColor: 0xff00ff,
-      //    debugStaticBodyColor: 0x0000ff,
-      //    debugVelocityColor: 0x00ff00,
-      //    maxEntries: 16,
-      //    useTree: true   // set false if amount of dynamic bodies > 5000
-    },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
+      scene: {
+        preload,
+        create,
+        update,
+      },
+      parent: gameContainer.current,
+    };
+
+    //
+    gameRef.current = new Phaser.Game(config);
+
+    // Clean up Phaser instance on unmount
+    return () => {
+      gameRef.current?.destroy(true);
+    };
+  }, []);
+
+  // Phaser scene functions
+  function preload(this: Phaser.Scene) {
+    this.load.image("background", "./assets/kenney_physics-assets/PNG/Backgrounds/blue_grass.png");
+    this.load.image("bird", "./assets/kenney_animal-pack-remastered/PNG/Round/bear.png");
+    this.load.image("pole", "./assets/kenney_physics-assets/PNG/Stone elements/elementStone017.png");
   }
-};
 
-new Phaser.Game(config)
+  function create(this: Phaser.Scene) {
+    this.add.image(500,300,"background")
+    // const bird = this.physics.add.sprite(500, 500, "bird");
+    // const poles = this.physics.add.group();
+    //dodać więcej logiki tutaj
+  }
 
-function preload(this: Phaser.Scene) {
-  this.load.image("background", "choose from assets")
-  this.load.image("bird", "choose from assets")
-  this.load.image("pole", "choose from assets")
-}
-
-/*
-gravity
-jump
-moving poles
-collision
-score
- */
-function create(this: Phaser.Scene) {
-  this.add.image(500, 500, "background")
-  bird = this.physics.add.sprite(500, 500, "bird")
-  poles = this.physics.add.group()
-
-  //GRAVITY
-  this.physics.world.gravity.x;
-  this.physics.world.gravity.y;
-
-
-
-}
-
-function update(this: Phaser.Scene) {
-
+  function update(this: Phaser.Scene) {
+    // Logika gry
+  }
+  //
+  return <div ref={gameContainer} style={{ width: 1000, height: 600}} />;
 }
