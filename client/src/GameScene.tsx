9bf46822f3229d5ea9c781b96e61c50f312c4583
MariuszADS <informatyka402@gmail.com>
2026-06-01 22:24:55 +0200

    cleaing code

--- client/src/GameScene.tsx
+++ client/src/GameScene.tsx
@@ -1,45 +1,25 @@
-import{ useEffect, useRef } from 'react';
 import Phaser from 'phaser';
 
 export function GameScene() {
 
-  // game_div
-  const gameContainer = useRef<HTMLDivElement>(null);
-
-  // game_instance
-  const gameRef = useRef<Phaser.Game | null>(null);
-
-  // if game_div exist retrun current game_instance
-  useEffect(() => {
-    if (!gameContainer.current) return;
-
-    const config: Phaser.Types.Core.GameConfig = {
-      type: Phaser.AUTO,
-      width: 1000,
-      height: 600,
-      physics: {
-        default: "arcade",
-        arcade: {
-          gravity: { y: 100, x: -10 },
-          debug: true,
-        },
+  const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 600,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 100, x: -10 },
+        debug: true,
       },
-      scene: {
-        preload,
-        create,
-        update,
-      },
-      parent: gameContainer.current,
-    };
+    },
+    scene: {
+      preload,
+      create,
+      update,
+    },
+  };
 
-    //
-    gameRef.current = new Phaser.Game(config);
-
-    // Clean up Phaser instance on unmount
-    return () => {
-      gameRef.current?.destroy(true);
-    };
-  }, []);
 
   // Phaser scene functions
   function preload(this: Phaser.Scene) {
@@ -49,15 +29,11 @@ export function GameScene() {
   }
 
   function create(this: Phaser.Scene) {
-    this.add.image(500,300,"background")
-    // const bird = this.physics.add.sprite(500, 500, "bird");
-    // const poles = this.physics.add.group();
-    //dodać więcej logiki tutaj
+    this.add.image(500, 300, "background")
   }
 
   function update(this: Phaser.Scene) {
-    // Logika gry
   }
-  //
-  return <div ref={gameContainer} style={{ width: 1000, height: 600}} />;
+  
 }
+
